32c675dc89091b388ef1a492f9ab70939234dbcf
OsisZool <jbmuthita@gmail.com>
2022-02-01 15:25:05 +0700

    tumraimairoo

---
+++ script.js
@@ -0,0 +1,6 @@
+var logo = document.querySelector('.container');
+var menu = document.querySelector('.sidebar');
+
+logo.addEventListener('click',function(){
+    menu.classList.toggle('showmenu');
+});
