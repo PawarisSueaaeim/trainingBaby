5b21b75249c1f7cac7ac12102f28bc2280b5e232
OsisZool <jbmuthita@gmail.com>
2022-02-02 15:26:38 +0700

    forresposive-70%

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var logo = document.querySelector('.container');
 var menu = document.querySelector('.sidebar');
 var content = document.querySelector('.content');
+var head = document.querySelector('sm-monitor');
 
 logo.addEventListener('click',function(){
-    menu.classList.toggle('hidemenu');
-    content.classList.toggle('content-full');
+    menu.classList.toggle('sidebar');
+    content.classList.toggle('hidecontent');
+    head.classList.toggle('sm-monitors');
+    // content.classList.toggle('content-full');
 });
+// logo.addEventListener('click',function(){
+//     menu.classList.toggle('hidemenu');
+//     content.classList.toggle('content-full');
+// });
