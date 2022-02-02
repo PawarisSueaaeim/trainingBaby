ef534dc0803511e15c19760630ce847c866208be
OsisZool <jbmuthita@gmail.com>
2022-02-02 08:51:52 +0700

    content

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var logo = document.querySelector('.container');
 var menu = document.querySelector('.sidebar');
+var content = document.querySelector('.content');
 
 logo.addEventListener('click',function(){
     menu.classList.toggle('hidemenu');
+    content.classList.toggle('content-full');
 });
